8bb7ba444015088d3d3a4afde0215e1edd28f413
hugosmoreira <hugo.n4@gmail.com>
2021-07-11 15:49:15 -0700

    Code and create website

---
+++ script.js
@@ -0,0 +1,48 @@
+const menu = document.querySelector(".menu");
+const navbar = document.querySelector(".navbar");
+
+menu.addEventListener("click", () => {
+  navbar.classList.toggle("change");
+  menu.classList.toggle("change");
+});
+
+const video = document.querySelector(".video");
+const btn = document.querySelector("button i");
+const bar = document.querySelector(".video-bar");
+
+const playPause = () => {
+  if (video.paused) {
+    video.play();
+    btn.className = "far fa-pause-circle";
+  } else {
+    video.pause();
+    btn.className = "far fa-play-circle";
+  }
+};
+
+btn.addEventListener("click", () => {
+  playPause();
+});
+
+video.addEventListener("timeupdate", () => {
+  const barWidth = video.currentTime / video.duration;
+  bar.style.width = `${barWidth * 100}%`;
+  if (video.ended) {
+    btn.className = "far fa-play-circle";
+    video.style.opacity = ".3";
+  }
+});
+
+var swiper = new Swiper(".swiper-container", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 70,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+});
